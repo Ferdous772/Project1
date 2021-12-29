8110b227b1057014abd88b4d15d10318ae1b763f
Ferdous772 <89370772+Ferdous772@users.noreply.github.com>
2021-12-29 22:37:30 +0600

    Project-1
    This is my First Project

---
+++ script.js
@@ -0,0 +1,5 @@
+function ourclick(){
+    document.getElementById('ctahead').innerHTML='This is our Change Heading';
+    document.getElementById('ctahead').style.color="#55ad6d";
+    document.getElementById('ctahead').style.fontSize="50px";
+}
